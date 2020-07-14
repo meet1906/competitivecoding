8cf4279a7590fc65d03f22f1a0336abd4d980e83
Meet Shah <meet19061999@gmail.com>
2020-07-14 21:40:59 +0530

    Array

---
+++ array.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+	int n,i;
+	int a[10];
+	for(i=0;i<5;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<5;i++)
+	{
+		printf("%d\n",a[i]);
+	}
+}
+	
