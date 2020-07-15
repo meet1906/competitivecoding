a414cf64e427133b8b78bbdd2177cbb757c6b35d
Meet Shah <meet19061999@gmail.com>
2020-07-15 21:13:50 +0530

    Insertionat desired pos

--- array/insert.c
+++ array/insert.c
@@ -2,7 +2,7 @@
 #include<stdlib.h>
 void main()
 {
-	int i,len,max,key;
+	int i,len,max,key,pos;
 	printf("Enter max size:\n");
 	scanf("%d",&max);
 	int a[max];
@@ -17,9 +17,26 @@ void main()
 	{
 		printf("Enter key:\n");
 		scanf("%d",&key);
-		//If insert is to happen at the end
-		a[len]=key;
+		/*If insert is to happen at the end
+		a[len]=key;*/
 		
+		//insert at a given position
+		printf("Enter position you want to enter");
+		scanf("%d",&pos);
+
+		/*if you use this then the elements gets overwritten with current pos val
+		for(i=pos;i<=len;i++)
+		{
+		a[i+1]=a[i];
+		}
+		a[pos]=key;
+		*/
+		//instead start from last
+		for(i=len-1;i>=pos;i--)
+		{
+			a[i+1]=a[i];
+		}
+		a[pos]=key;
 		for(i=0;i<=len;i++)
 		{
 			printf("%d\n",a[i]);
