171300434e1f3d6174372deb152d15156e65ec93
Meet Shah <meet19061999@gmail.com>
2020-08-01 20:56:23 +0530

    Anti Clockwise Rotation of Array

---
+++ array/rotation.c
@@ -0,0 +1,38 @@
+//Program to perform rotation of array
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+	int size;
+	scanf("%d",&size);
+	int a[100000];
+	int i;
+	for(i=0;i<size;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	printf("\n");
+	int k;
+	scanf("%d",&k);
+	int temp[1000];
+	for(i=0;i<k;i++)
+	{
+		temp[i]=a[i];
+	}
+	//shifting all the elements after k to the left by k positions
+	for(i=k;i<size;i++)
+	{
+		a[i-k]=a[i];
+	}
+	int j=0;
+	for(i=size-k;i<size;i++)
+	{
+		a[i]=temp[j++];
+
+	}
+	printf("\n Rotated Array: \n");
+	for(i=0;i<size;i++)
+	{
+		printf("%d\n",a[i]);
+	}
+}
