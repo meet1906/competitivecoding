1b0d53a12ee43ef633bef6616d548534c915e027
meet1906 <meet19061999@gmail.com>
2020-12-23 19:56:39 +0530

    inc array size

---
+++ udemy/arrays/increasearray_size.cpp
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    int *p;
+    int *q;
+    p = new int(5);
+    q = new int(10);
+    
+    p=NULL;
+    p=q;
+    q=NULL;
+    //P IS OF SIZE 10 NOW!!
+}
